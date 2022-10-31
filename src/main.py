1b44e8c1eee734b4139bf173ecc605d5e6edb88a
Lari Liuhamo <lari.liuhamo@gmail.com>
2022-10-31 12:48:48 +0200

    [PATCH] Added traffic light code

--- src/main.py
+++ src/main.py
@@ -1,10 +1,51 @@
 import utime
 from machine import Pin
 
-led = Pin(28, Pin.OUT)
-led.low()
+
+class Pins:
+    def __init__(self):
+        self.RED = Pin(30, Pin.OUT)
+        self.YELLOW = Pin(29, Pin.OUT)
+        self.GREEN = Pin(28, Pin.OUT)
+        
+        self.RED.low()
+        self.YELLOW.low()
+        self.GREEN.high()
+
+    def green_to_red(self):
+        if self.RED.value() == 1:
+            return
+        
+        self.GREEN.high()
+        # utime.sleep(5)
+        self.GREEN.low()
+        self.YELLOW.high()
+        utime.sleep(1)
+
+        self.YELLOW.low()
+        self.RED.high()
+        utime.sleep(5)
+
+    def red_to_green(self):
+        if self.GREEN.value() == 1:
+            return
+
+        self.YELLOW.high()
+        utime.sleep(1)
+
+        self.RED.low()
+        self.YELLOW.low()
+        self.GREEN.high()
+        utime.sleep(5)
+
+    
+
+pins = Pins()
+
+
 
 while True:
-    led.toggle()
-    print("Toggle")
-    utime.sleep(1)
+    pins.green_to_red()
+    print("Toggled to red")
+    pins.red_to_green()
+    print("Toggled to green")
