47212bab2e907ac1a11ef5cb867915fd8b116700
Lari Liuhamo <lari.liuhamo@gmail.com>
2022-11-21 15:05:11 +0200

    [PATCH] LCD confirmed to work

--- src/main.py
+++ src/main.py
@@ -1,16 +1,22 @@
-from machine import I2C, Pin
+from machine import I2C, Pin, SoftI2C
 from time import sleep
-from pico_i2c_lcd import I2cLcd
+from grove_lcd_i2c import Grove_LCD_I2C
 
 i2c = I2C(0, sda=Pin(0), scl=Pin(1), freq=400_000)
+# i2c = SoftI2C(sda=Pin(0), scl=Pin(1), freq=400_000)
 
 I2C_ADDR = i2c.scan()[0]
+print(i2c.scan())
 
-lcd = I2cLcd(i2c, I2C_ADDR, 2, 16)  # rows, cols
+lcd = Grove_LCD_I2C(i2c, I2C_ADDR)
+lcd.home()
 
 while True:
     print(I2C_ADDR)
-    lcd.blink_cursor_on()
-    lcd.putstr(f"I2C Address: {I2C_ADDR}\n")
-    lcd.putstr("Shadow Garden")
+    lcd.write(f"I2C Address: {I2C_ADDR}\nShadow Garden")
     sleep(2)
+    lcd.clear()
+    lcd.home()
+    lcd.write(f"I2C Address: {I2C_ADDR:x}\nShadow Garden")
+    sleep(2)
+    lcd.clear()
