cc5d7f720b98f2750e7bf97be09fcd7e0e5d1d2e
Lari Liuhamo <lari.liuhamo@gmail.com>
2022-11-21 13:03:10 +0200

    [PATCH] Added LCD code

--- src/main.py
+++ src/main.py
@@ -1,10 +1,16 @@
-import utime
-from machine import Pin
+from machine import I2C, Pin
+from time import sleep
+from pico_i2c_lcd import I2cLcd
 
-led = Pin(28, Pin.OUT)
-led.low()
+i2c = I2C(0, sda=Pin(0), scl=Pin(1), freq=400_000)
+
+I2C_ADDR = i2c.scan()[0]
+
+lcd = I2cLcd(i2c, I2C_ADDR, 2, 16)  # rows, cols
 
 while True:
-    led.toggle()
-    print("Toggle")
-    utime.sleep(1)
+    print(I2C_ADDR)
+    lcd.blink_cursor_on()
+    lcd.putstr(f"I2C Address: {I2C_ADDR}\n")
+    lcd.putstr("Shadow Garden")
+    sleep(2)
